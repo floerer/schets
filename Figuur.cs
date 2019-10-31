05278d62390f0a4fb6106fd8e4ae10e23438911c
floerer <flovlist1@gmail.com>
2019-10-31 21:46:20 +0100

    Add files via upload

---
+++ Figuur.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchetsEditor
+{
+    class Figuur
+    {
+        private int xMin;
+        private int yMin;
+        private int xMax;
+        private int yMax;
+        private int kleur;
+        private StartpuntTool soort;
+
+
+        public Figuur(int xm, int xM, int ym, int yM, int kl, StartpuntTool soortFiguur)
+        {
+            xMin = xm;
+            xMax = xM;
+            yMin = ym;
+            yMax = yM;
+            kleur = kl;
+            soort = soortFiguur;
+        }
+
+        public bool isRaak(int x, int y)
+        {
+            if(x < xMax && x > xMin && y < yMax && y > yMin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
